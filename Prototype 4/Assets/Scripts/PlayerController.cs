c57f2b13b49d25110048784a3c6cc6d57ca6136f
Seb-deru <derusebastien@gmail.com>
2025-05-19 20:19:59 +0200

    feat: added powerup

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
+using System.Collections;
+using TreeEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
+    private float powerupStrength = 15;
+    public bool hasPowerup = false;
     public float speed = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,5 +23,35 @@ public class PlayerController : MonoBehaviour
     {
         float verticalInputs = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward *  verticalInputs * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup")) {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Collided width: " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+        }
     }
 }
